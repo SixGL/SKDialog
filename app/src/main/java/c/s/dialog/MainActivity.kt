5cde641c754f2324d2052dae54532ac3a73b61cf
Six <254369044@qq.com>
2019-12-23 13:53:13 +0800

    1、增加简洁的loading界面布局 2、增加以Alertdialog为基础的loading布局

--- app/src/main/java/c/s/dialog/MainActivity.kt
+++ app/src/main/java/c/s/dialog/MainActivity.kt
@@ -4,12 +4,15 @@ import android.app.Activity
 import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.constraint.ConstraintLayout
 import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import c.s.d.SDialog
+import c.s.d.SLoadingAlertDialog
 import c.s.d.listener.DismissLitener
 import c.s.d.listener.LogicListener
 
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         activitys = this
 
     }
+
 
     fun showDialog(v: View) {
         val dialog = SDialog.Builder()
@@ -59,5 +63,46 @@ class MainActivity : AppCompatActivity() {
                     }
                 })// 用于dialog弹出后，处理dialog内部业务逻辑
                 .show()// 弹出dialog，放置最后调用
+    }
+
+    fun showLoadingDialog(v: View) {
+        val dialog = SDialog.Builder()
+                .setContentView(R.layout.s_dialog_loading)// dialog 布局
+                .setAnimation(0) // 弹出动画
+                .setDialogOutTransparency(0.3f)// 弹出框外 背景透明度
+                .setFramentManager(supportFragmentManager) //FragmentManager
+                .addDismissListener(object : DismissLitener {
+                    override fun dismissCallback(dialog: DialogInterface?) {
+                        Log.e("dismissCallback", "dismissCallback")
+                    }
+                })// dialog关闭回调
+                .addLogicListener(object : LogicListener {
+                    override fun logicCallback(view: View?) {
+                        view?.findViewById<TextView>(R.id.s_tv_loading)?.text = "正在加载..."
+                        view?.findViewById<ConstraintLayout>(R.id.s_ct_loading)?.setBackgroundResource(R.drawable.s_shape_loading_test)
+                    }
+                })// 用于dialog弹出后，处理dialog内部业务逻辑
+                .show()// 弹出dialog，放置最后调用
+
+    }
+
+    var showAlert: SLoadingAlertDialog? = null
+    fun showAlertLoadingDialog(v: View) {
+        // 默认简洁的AlertDialog，不需要FragmentManager
+        showAlert = SLoadingAlertDialog()
+                .with(this)
+                .setContentView(R.layout.s_dialog_loading)
+                .setHeight(90)
+                .setWidth(90)
+                .setCancelable(true)
+                .setDialogOutTransparency(0.5f)
+                .show()
+
+
+        Handler().postDelayed(Runnable {
+            showAlert?.getAlertDialog()?.dismiss()
+        },3000)
+
+
     }
 }
---
+++ lib_kdialog/src/main/java/c/s/d/SLoadingAlertDialog.kt
@@ -0,0 +1,83 @@
+package c.s.d
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.util.TypedValue
+import android.view.LayoutInflater
+
+
+class SLoadingAlertDialog {
+    private var mActivity: Activity? = null
+    private var mWidth: Int = 90 //dp
+    private var mHeight: Int = 90
+    private var mLayoutId: Int = 0
+    private var mCancelable: Boolean = true
+    private var mDialogOutTransparency: Float = 0.3f
+    private var dialog: AlertDialog? = null
+
+
+    fun getAlertDialog(): AlertDialog? {
+        return dialog
+    }
+
+    fun with(mAc: Activity): SLoadingAlertDialog {
+        mActivity = mAc
+        return this
+    }
+
+    fun setWidth(width: Int): SLoadingAlertDialog {
+        mWidth = width
+        return this
+    }
+
+    fun setHeight(height: Int): SLoadingAlertDialog {
+        mHeight = height
+        return this
+    }
+
+
+    fun setContentView(layoutId: Int): SLoadingAlertDialog {
+        mLayoutId = layoutId
+        return this
+    }
+
+    /**
+     * 设置Dialog框以外的背景透明度
+     * 默认0.5f
+     * @param transparency 0 - 1f
+     * */
+    fun setDialogOutTransparency(transparency: Float): SLoadingAlertDialog {
+        mDialogOutTransparency = transparency
+        return this
+    }
+
+
+    /**
+     * 屏蔽触摸dialog框以外屏幕、返回键  关闭dialog
+     * @param cancelable
+     * */
+    fun setCancelable(cancelable: Boolean): SLoadingAlertDialog {
+        mCancelable = cancelable
+        return this
+    }
+
+    fun show(): SLoadingAlertDialog {
+        val inflate = LayoutInflater.from(mActivity).inflate(R.layout.s_dialog_loading, null)
+        dialog = AlertDialog.Builder(mActivity)
+                .setView(inflate)
+                .setCancelable(mCancelable)
+                .show()
+
+        val params = dialog?.getWindow()!!.getAttributes()
+        params.width = dp2px(mWidth.toFloat())
+        params.height = dp2px(mHeight.toFloat())
+        dialog?.getWindow()?.setAttributes(params)
+        dialog?.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent)
+        return this
+    }
+
+    private fun dp2px(dpVal: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, mActivity?.getResources()?.getDisplayMetrics()).toInt()
+    }
+}
